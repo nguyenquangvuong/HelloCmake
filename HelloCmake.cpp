9b1e7977efa465d91334970b775a08a63b61cf59
nguyenquangvuong <51113359+nguyenquangvuong@users.noreply.github.com>
2020-05-16 10:53:19 +0700

    Add files via upload

---
+++ HelloCmake.cpp
@@ -0,0 +1,19 @@
+//HelloCmake
+//Learn Cmake
+//
+// Created by Nguyen Quang Vuong on 16/05/2020
+//Copyrighted © 2020 Nguyen Quang Vuong. All rights reserved
+//
+#include<stdio.h>
+
+void greeting_en()
+{
+printf("I have to learn good Cmake !!!\n");
+}
+
+int main(int argc, char* argv[])
+{
+greeting_en();
+
+return 0;
+}
